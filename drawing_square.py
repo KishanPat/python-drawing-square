94717b28a73ead21fd0eaac3ba214712940f7407
KishanPat <67847918+KishanPat@users.noreply.github.com>
2020-08-04 21:50:42 -0400

    Initial commit

---
+++ drawing_square.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+# options for sizing square
+size=350
+rightAngle=90
+
+# drawing the square
+forward(size)
+right(rightAngle)
+forward(size)
+right(rightAngle)
+forward(size)
+right(rightAngle)
+forward(size)
